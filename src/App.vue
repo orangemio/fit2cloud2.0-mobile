18995f0fb402280bd4d5b14bde883d8393e4ec08
OrangeM21 <orangemtony@gmail.com>
2019-05-24 18:11:27 +0800

    添加i18n标题

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
   <div id="app">
+    <x-header :right-options="{showMore: true}" @on-click-more="showMenus = true">ACloud</x-header>
     <router-view></router-view>
+    <div v-transfer-dom>
+      <actionsheet :menus="menus" v-model="showMenus" show-cancel></actionsheet>
+    </div>
   </div>
 </template>
 
+
 <script>
+import { XHeader, Actionsheet, TransferDom, ButtonTab, ButtonTabItem } from 'vux'
+
 export default {
+  directives: {
+    TransferDom
+  },
+  components: {
+    XHeader,
+    Actionsheet,
+    ButtonTab,
+    ButtonTabItem
+  },
+  data () {
+    return {
+      menus: {
+        menu1: '中文',
+        menu2: 'English'
+      },
+      showMenus: false
+    }
+  },
   name: 'app'
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // 引用自定义组件
 import Home from '@/components/HelloFromVux'
+import login from '@/components/login'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
     }
   ]
 })
