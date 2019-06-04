9138496ea880815247590210b04b294016f1bd17
maguohao2018 <guohao.ma@fit2cloud.com>
2019-06-04 15:46:34 +0800

    apply

--- src/components/Apply.vue
+++ src/components/Apply.vue
@@ -3,8 +3,10 @@
      <tab>
          <tab-item v-for="n in list2" :key="n" :selected="n==='Tiexi'" @on-item-click="onItemClick">{{ n }}</tab-item>
      </tab>
-     <br/>
 
+     <div style="margin-top:10px;">
+         <form-preview :header-label="$t('platform')" header-value="plugin" :body-items="list" :footer-buttons="buttons2" name="demo"></form-preview>
+     </div>
   </div>
 </template>
 
@@ -22,21 +24,48 @@ Lab:
   zh-CN: Lab
 Aws:
   zh-CN: Aws
+platform:
+  zh-CN: 平台
+apply:
+  zh-CN: 申请
 </i18n>
 
 <script>
-import { Tab, TabItem } from 'vux'
+import { Tab, TabItem, FormPreview } from 'vux'
 const list = () => ['Tiexi', 'Dadong', 'Beijing', 'Azure', 'Lab', 'Aws']
 
 export default {
   components: {
     Tab,
-    TabItem
+    TabItem,
+    FormPreview
   },
   data () {
     return {
+      list: [{
+        label: '',
+        value: 'CentOS 7.3'
+      }, {
+        label: '',
+        value: 'for CL3'
+      }, {
+        label: '标题标题',
+        value: '很长很长的名字很长很长的名字很长很长的名字很长很长的名字很长很长的名字'
+      }],
+      buttons2: [{
+        style: 'primary',
+        text: this.$t('apply'),
+        onButtonClick: (name) => {
+          alert(`clicking ${name}`)
+        }
+      }],
       list2: list()
     }
+  },
+  created () {
+    this.$http.get('/http://103.235.232.207/vm-service/catalog-product/list').then(({data}) => {
+      console.log(data)
+    })
   },
   methods: {
     onItemClick (index) {
