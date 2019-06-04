45bb89b6965daaa1507cb1ddf41a7f301fc41795
maguohao2018 <guohao.ma@fit2cloud.com>
2019-06-04 10:33:35 +0800

    产品标签

---
+++ src/components/Apply.vue
@@ -0,0 +1,124 @@
+<template>
+  <div >
+     <tab>
+         <tab-item v-for="n in list2" :key="n" :selected="n==='Tiexi'">{{ n }}</tab-item>
+     </tab>
+     <br/>
+
+  </div>
+</template>
+
+
+<i18n>
+Tiexi:
+  zh-CN: 铁西
+Dadong:
+  zh-CN: 大东
+Beijing:
+  zh-CN: 北京
+Azure:
+  zh-CN: Azure
+Lab:
+  zh-CN: Lab
+Aws:
+  zh-CN: Aws
+</i18n>
+
+<script>
+import { Tab, TabItem, Sticky, Divider, XButton, Swiper, SwiperItem } from 'vux'
+const list = () => ['Tiexi', 'Dadong', 'Beijing', 'Azure', 'Lab', 'Aws']
+
+export default {
+  components: {
+    Tab,
+    TabItem,
+    Sticky,
+    Divider,
+    XButton,
+    Swiper,
+    SwiperItem
+  },
+  data () {
+    return {
+      index01: 0,
+      list2: list(),
+      demo2: '美食',
+      list3: ['收到的消息', '发出的消息'],
+      demo3: '收到的消息',
+      list4: ['正在放映', '即将上映'],
+      demo4: '即将上映',
+      demoDisabled: 'A',
+      index: 0,
+      getBarWidth: function (index) {
+        return (index + 1) * 22 + 'px'
+      }
+    }
+  },
+  methods: {
+    switchTabItem (index) {
+      console.log('on-before-index-change', index)
+      this.$vux.loading.show({
+        text: 'loading'
+      })
+      setTimeout(() => {
+        this.$vux.loading.hide()
+        this.index01 = index
+      }, 1000)
+    },
+    onItemClick (index) {
+      console.log('on item click:', index)
+    },
+    addTab () {
+      if (this.list2.length < 5) {
+        this.list2 = list().slice(0, this.list2.length + 1)
+      }
+    },
+    removeTab () {
+      if (this.list2.length > 1) {
+        this.list2 = list().slice(0, this.list2.length - 1)
+      }
+    },
+    next () {
+      if (this.index === this.list2.length - 1) {
+        this.index = 0
+      } else {
+        ++this.index
+      }
+    },
+    prev () {
+      if (this.index === 0) {
+        this.index = this.list2.length - 1
+      } else {
+        --this.index
+      }
+    }
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+@import '~vux/src/styles/1px.less';
+@import '~vux/src/styles/center.less';
+
+.box {
+  padding: 15px;
+}
+.active-6-1 {
+  color: rgb(252, 55, 140) !important;
+  border-color: rgb(252, 55, 140) !important;
+}
+.active-6-2 {
+  color: #04be02 !important;
+  border-color: #04be02 !important;
+}
+.active-6-3 {
+  color: rgb(55, 174, 252) !important;
+  border-color: rgb(55, 174, 252) !important;
+}
+.tab-swiper {
+  background-color: #fff;
+  height: 100px;
+}
+</style>
+
