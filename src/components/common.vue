81d9a9452adbe9fd3a5fc3fa0b55776bd8c0c404
OrangeM21 <orangemtony@gmail.com>
2019-06-05 11:15:52 +0800

    fix

---
+++ src/components/common.vue
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+    var CryptoJS = require('crypto-js')
+
+    // 定义一些公共的属性和方法
+    // const AccessKey = '7fGeaovhswFRcWfc'
+    const SecretKey = 'u82P9DsxIwxOG8ZC'
+    // const loginUrl = 'http://103.235.232.207/'
+    function gethashkey () {
+      console.log(CryptoJS.enc.Utf8.parse(SecretKey))
+    }
+    // 暴露出这些属性和方法
+    export default {
+      gethashkey
+    }
+</script>
