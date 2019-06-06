a56b87b592f14dc4e748c84ffc6db0fc74db1aa9
OrangeM21 <orangemtony@gmail.com>
2019-06-06 12:01:06 +0800

    fix

--- src/components/common.vue
+++ src/components/common.vue
@@ -16,21 +16,9 @@
         }).toString()
       return SecretKeySpec
     }
-    // function getInfo () {
-    //   this.$http.get('http://103.235.232.207/dashboard/user/info', {
-    //     headers: {
-    //       accept: 'application/json',
-    //       accessKey: '7fGeaovhswFRcWfc',
-    //       signature: 'g1W6lRfYAtE0UsIYAC6Q2FAi1oBfGK164DxIhQJXwU3NsXOskr/qkMjT7Yqnbaxa2vVFf3BUGT34JMM/TXv5d/iSNBrn4jGxbYJHA7Jo59E='
-    //     }
-    //   }).then(({data}) => {
-    //     console.log(data)
-    //   })
-    // }
 
     // 暴露出这些属性和方法
     export default {
       gethashkey
-      // getInfo
     }
 </script>
