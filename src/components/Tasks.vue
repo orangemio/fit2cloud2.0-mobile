e01ffbf8633ff68c946abe65a5fe37b6eb9edb70
OrangeM21 <orangemtony@gmail.com>
2019-06-06 17:14:55 +0800

    fix

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,17 +1,61 @@
 <template>
-    <x-button @click.native='showkey()'>submit</x-button>
+    <div>
+    <x-button @click.native='showkey()'>获取最新Tag</x-button>
+     <form-preview :header-label="$t('付款金额')" header-value="¥2400.00" :body-items="list" :footer-buttons="buttons1"></form-preview>
+    </div>
 </template>
 
+<i18n>
+付款金额:
+  en: Total
+标题标题:
+  en: Item Title
+商品:
+  en: Product
+很长很长的名字很长很长的名字很长很长的名字很长很长的名字很长很长的名字:
+  en: Long Long Long Long Long Long Long Long Long Long Long Long Content
+名字名字名字:
+  en: Name Name
+电动打蛋机:
+  en: Item title
+辅助操作:
+  en: Info
+操作:
+  en: Action
+点击事件:
+  en: Click Event
+跳转到首页:
+  en: Homepage
+</i18n>
+
 <script>
-import { XButton } from 'vux'
+import { XButton, FormPreview } from 'vux'
 export default {
   data () {
     return {
-
+      list: [{
+        label: '商品',
+        value: '电动打蛋机'
+      }, {
+        label: '标题标题',
+        value: '名字名字名字'
+      }, {
+        label: '标题标题',
+        value: '很长很长的名字很长很长的名字很长很长的名字很长很长的名字很长很长的名字'
+      }],
+      buttons1: [{
+        style: 'default',
+        text: '辅助操作'
+      }, {
+        style: 'primary',
+        text: this.$t('跳转到首页'),
+        link: '/'
+      }]
     }
   },
   components: {
-    XButton
+    XButton,
+    FormPreview
   },
   methods: {
     showkey () {
@@ -26,7 +70,7 @@ export default {
           'signature': signature
         }
       }).then(({data}) => {
-        console.log(data)
+        this.list = 
       })
     }
   }
