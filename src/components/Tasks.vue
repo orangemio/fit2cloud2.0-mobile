24378e99f54078ce2331a688d2eb900ae5ffdb30
OrangeM21 <orangemtony@gmail.com>
2019-06-08 01:51:20 +0800

    pull

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,84 +1,58 @@
 <template>
     <div>
-    <x-button @click.native='showkey()'>获取最新Tag</x-button>
-    <form-preview :header-label="$t('付款金额')" header-value="¥2400.00" :body-items="list" :footer-buttons="buttons1"></form-preview>
+      <br>
+      <form-preview  :body-items="list" :footer-buttons="buttons1"></form-preview>
+      <br>
+      <form-preview  :body-items="list" :footer-buttons="buttons1"></form-preview>
     </div>
 </template>
 
 <i18n>
-付款金额:
-  en: Total
-标题标题:
-  en: Item Title
-商品:
-  en: Product
-很长很长的名字很长很长的名字很长很长的名字很长很长的名字很长很长的名字:
-  en: Long Long Long Long Long Long Long Long Long Long Long Long Content
-名字名字名字:
-  en: Name Name
-电动打蛋机:
-  en: Item title
-辅助操作:
-  en: Info
-操作:
-  en: Action
-点击事件:
-  en: Click Event
-跳转到首页:
-  en: Homepage
+  申请人:
+    en : Applicant
+  开始时间:
+    en : Start Time
+  拒绝:
+    en : Reject
+  查看:
+    en : Process
 </i18n>
 
 <script>
 // import http from '@/utils/http'
-import { XButton } from 'vux'
-import { XButton, FormPreview } from 'vux'
+import { FormPreview } from 'vux'
 export default {
   data () {
     return {
       list: [{
-        label: '商品',
-        value: '电动打蛋机'
+        label: this.$t('申请人'),
+        value: ''
       }, {
-        label: '标题标题',
-        value: '名字名字名字'
-      }, {
-        label: '标题标题',
-        value: '很长很长的名字很长很长的名字很长很长的名字很长很长的名字很长很长的名字'
+        label: this.$t('开始时间'),
+        value: ''
       }],
       buttons1: [{
         style: 'default',
-        text: '辅助操作'
+        text: this.$t('拒绝')
       }, {
         style: 'primary',
-        text: this.$t('跳转到首页'),
-        link: '/'
+        text: this.$t('查看'),
+        onButtonClick: (name) => {
+          alert(`clicking ${name}`)
+        }
       }]
     }
   },
   components: {
-    XButton,
     FormPreview
   },
   methods: {
-    showkey () {
-      // 获取最新签名
-      var signature = this.COMMON.gethashkey('u82P9DsxIwxOG8ZC', '7fGeaovhswFRcWfc')
-      // Demo 从注册到全局的AXIOS中发起Get请求
-      this.$http.get('/api/vm-service/tag/listAll', {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'accept': 'application/json',
-          'accessKey': '7fGeaovhswFRcWfc',
-          'signature': signature
-        }
-      }).then(({data}) => {
-        this.list = 
-      })
-    }
   }
 }
 </script>
 
 <style lang="less">
-    
+    .weui-form-preview__hd{
+      display:none;
+    }
 </style>
